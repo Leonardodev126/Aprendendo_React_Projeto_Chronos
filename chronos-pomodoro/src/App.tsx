aacaccfe8f7fb79ec5a1fe7ee0d9be817b50f8f9
Leo Keven <leodeveloper12@gmail.com>
2025-06-10 16:56:46 -0300

    Finalizando o componente Heading e tipagem TypeScript de children (ReactNode)

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,12 +1,16 @@
 import "./styles/theme.css"
 import "./styles/global.css"
 import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react"
 
 
 export function App() {
   return (
     <>
-    <Heading attr={123} attr2="String">Olá mundo</Heading>
+    <Heading>
+      Olá mundo
+      <button><TimerIcon /></button>
+    </Heading>
     <h1>Olá pessoal</h1>
     </>
   );
