be01e7fabb683e33af62a774bb5425d91342d59d
Leo Keven <leodeveloper12@gmail.com>
2025-06-12 19:43:51 -0300

    Contruindo o forms do layout

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,26 +1,50 @@
-import { Container } from "./components/Container"
+import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import "./styles/theme.css";
 import "./styles/global.css";
 
-
 export function App() {
   return (
     <>
-    <Container>
-      <Logo>LOGO</Logo>
-    </Container>
+      <Container>
+        <Logo>LOGO</Logo>
+      </Container>
 
-    <Container>
-      <Menu />
-    </Container>
+      <Container>
+        <Menu />
+      </Container>
 
-    <Container>
-      <CountDown />
-    </Container>
+      <Container>
+        <CountDown />
+      </Container>
 
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='input'>task</label>
+            <input id='input' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+
+
+        </form>
+      </Container>
     </>
   );
 }
