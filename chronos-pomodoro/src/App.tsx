2b1e8534579262930ed8a76ac64dfbd8d95efac5
Leo Keven <leodeveloper12@gmail.com>
2025-06-17 09:47:34 -0300

    Capturando o evento de clique em link com preventDefault para terma dark e light

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -9,25 +9,12 @@ import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { PlayCircleIcon } from "lucide-react";
 import { Footer } from "./components/Footer";
-import { Heading } from "./components/Heading";
-import { useState } from "react";
+// import { Heading } from "./components/Heading";
+// import { useState } from "react";
 
 export function App() {
-  // Todos os componentes que usam número, saibam das mudanças em seu valor.
-  const [numero, setNumero] = useState(0);
-
-  // Sempre que eu usar useState, não vou usar atribuição diretamente
-  function handleClick() {
-    
-    // setNumero((prevState) => prevState + 1); Se precisar usar o valor anterior
-    setNumero(numero + 1);
-  }
-
   return (
     <>
-      <Heading>Número: <span id="numero">{numero}</span></Heading>
-      <button onClick={handleClick}>Aumenta</button>
-
       <Container>
         <Logo>LOGO</Logo>
       </Container>
@@ -43,7 +30,7 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput labelText={numero.toString()} id="input" type="text" required placeholder="Digite algo" />
+            <DefaultInput labelText="task" id="input" type="text" required placeholder="Digite algo" />
           </div>
 
           <div className='formRow'>
--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,21 +1,32 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from "lucide-react";
 import styles from "./styles.module.css";
+import { useState } from "react";
+
+type AvaliableThemes = "dark" | "light"
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvaliableThemes>("dark")
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault() // Tirando o comportamento padrão do link
+    console.log("Clicado")
+  }
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <h1>{theme}</h1>
+      <a className={styles.menuLink} href='#' aria-label="Ir para a Home" title="Ir para a Home">
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label="Ver histórico" title="Ver histórico">
         <HistoryIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label="Configurações" title="Configurações">
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label="Mudar tema" title="Mudar tema" onClick={handleThemeChange}>
         <SunIcon />
       </a>
     </nav>
